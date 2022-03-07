0eda96a4f0f80b4c477328e0ad37ccee69353c78
marj12 <100178324+marj12@users.noreply.github.com>
2022-03-07 10:33:54 +0800

    Assignment

---
+++ Lecture1/Reading Assignment/assignment_lec1_c2.c
@@ -0,0 +1,14 @@
+//Lecture 1 - 3
+
+#include <stdio.h>
+
+int main(void){
+    
+    int minuend, subtrahend, difference;
+
+    minuend = 87;
+    subtrahend = 15;
+    difference = minuend - subtrahend;
+
+    printf("The difference of %d and %d is %d.", minuend, subtrahend, difference);
+}
