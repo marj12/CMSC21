3801bf7bccc649244ff74afe73bc40069fcc6c99
marj12 <100178324+marj12@users.noreply.github.com>
2022-04-22 15:06:39 +0800

    Long Exam

--- Long Exam 1/le8.c
+++ Long Exam 1/le8.c
@@ -7,27 +7,29 @@ int main(void){
     int a,b;
 
     printf("Enter value of a:");
-    scanf("%d", &a); 
+    scanf("%d", &a); //enter value of a
 
     printf("Enter value of b:");
-    scanf("%d", &b);
+    scanf("%d", &b); //enter value of b
+
+    //use if-else statements to easily produce the outputs needed.
 
     //A
-    if (a==2 && b==3){
-        printf( "a. *****, >>>>>, <<<<<\n" );
+    if (a==2 && b==3){ //first output //compare
+        printf( "a. *****, >>>>>, <<<<<\n" ); //don't forget to use  escape character so the next output is printed on the next line
     }else{ 
         printf( "-----, >>>>>, <<<<<\n" );
     }
 
     //B
-    if (a==2 && b==3){
+    if (a==2 && b==3){ //second output
         printf( "b. *****\n" );
     }else{ 
         printf( "-----, >>>>>, <<<<<\n" );
     }
 
     //C
-    if (a==2 && b==3){
+    if (a==2 && b==3){ //third output
         printf( "c. *****, <<<<<\n" );
     }else{ 
         printf( "-----, >>>>>, <<<<<\n" );
